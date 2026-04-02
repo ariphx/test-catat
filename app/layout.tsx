f2c224abd8f63b6718212cb23030e6e202affdcc
ARIPPP <0.tiny.people@gmail.com>
2026-04-02 12:00:34 +0700

    fix: tutup tag html dan tambahkan children di layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,10 +18,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html
-      lang="en"
+    <html lang="en">
       <body className={`${inter.className} antialiased`}>
-</body>
+        {children}
+      </body>
     </html>
   );
 }
